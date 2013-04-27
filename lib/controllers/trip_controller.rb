681a55bb3cd5e26cbf85a7be10adc10d3fe4cd44
Daniel Zurawski <zurawski.daniel@gmail.com>
2013-04-27 20:44:07 +0100

    Changed the single route to a GET to prevent from this shitty cross domain issue

--- lib/controllers/trip_controller.rb
+++ lib/controllers/trip_controller.rb
@@ -4,8 +4,8 @@ module YTravel
 			erb :index, layout: true
 		end
 
-		post '/plan' do 
-			LandmarkController.new.find_landmarks(params).to_json
+		get '/plan/:lat/:long/:start_date/:end_date' do |lat, long, start_date, end_date|
+			LandmarkController.new.find_landmarks({:lat => lat.to_f, :long => long.to_f, :start_date => start_date.to_i, :end_date => end_date.to_i}).to_json
 		end
 	end
 end
